f15215efad7519c099015d399d26690736f64b47
Jacky-Acc <majielong970325@gmail.com>
2026-08-07 15:37:38 +0800

    Fix post processing module error

--- src/PostProcessing.js
+++ src/PostProcessing.js
@@ -1,29 +1,3 @@
-import * as THREE from
-"https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
-
-
-import {
-EffectComposer
-}
-from
-"https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/postprocessing/EffectComposer.js";
-
-
-import {
-RenderPass
-}
-from
-"https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/postprocessing/RenderPass.js";
-
-
-import {
-UnrealBloomPass
-}
-from
-"https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/postprocessing/UnrealBloomPass.js";
-
-
-
 export default class PostProcessing {
 
 
@@ -34,52 +8,7 @@ camera
 ){
 
 
-this.composer =
-new EffectComposer(
-renderer
-);
-
-
-
-this.composer.addPass(
-
-new RenderPass(
-
-scene,
-
-camera
-
-)
-
-);
-
-
-
-const bloom =
-new UnrealBloomPass(
-
-new THREE.Vector2(
-
-window.innerWidth,
-
-window.innerHeight
-
-),
-
-0.8,
-
-0.4,
-
-0.85
-
-);
-
-
-
-this.composer.addPass(
-bloom
-);
-
+this.renderer = renderer;
 
 
 }
@@ -89,10 +18,14 @@ bloom
 render(){
 
 
-this.composer.render();
+this.renderer.render(
+    this.scene,
+    this.camera
+);
 
 
 }
+
 
 
 }
